6c5f9c6ce101a43c6e948016f1f096ab428da232
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-07-31 15:45:11 +0500

    UPD get_wallets algorithm

--- src/file_manager.py
+++ src/file_manager.py
@@ -4,16 +4,15 @@ import json
 from typing import Union, List
 
 from src.paths import (APTOS_WALLETS_FILE,
-                       EVM_ADDRESSES_FILE)
+                       EVM_ADDRESSES_FILE,
+                       PROXY_FILE)
 from src.schemas.wallet_data import (WalletData,
                                      ProxyData)
 
 
 class FileManager:
-    wallet_length: int
-
-    def __init__(self):
-        self.wallet_length = 66
+    APTOS_KEY_LENGTH = 66
+    EVM_ADDRESS_LENGTH = 42
 
     def read_data_from_txt_file(self, file_path) -> Union[list, None]:
         if not os.path.exists(file_path):
@@ -37,58 +36,62 @@ class FileManager:
         with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
 
-    def get_wallets(self,
-                    aptos_wallets_file_data=None,
-                    evm_addresses_file_data=None) -> Union[List[WalletData], None]:
-        if not aptos_wallets_file_data:
-            aptos_wallets_data = self.read_data_from_txt_file(APTOS_WALLETS_FILE)
-            evm_addresses_data = self.read_data_from_txt_file(EVM_ADDRESSES_FILE)
+    def _extract_valid_proxy_data_from_str(self, proxy_data: str) -> Union[List[ProxyData], None]:
+        if not proxy_data:
+            return None
+
+        proxy_data = proxy_data.split(":")
+        if len(proxy_data) == 2:
+            host, port = proxy_data
+            proxy_data = ProxyData(host=host,
+                                   port=port)
+
+        elif len(proxy_data) == 4:
+            host, port, username, password = proxy_data
+            proxy_data = ProxyData(host=host,
+                                   port=port,
+                                   username=username,
+                                   password=password,
+                                   auth=True)
+
         else:
-            aptos_wallets_data = aptos_wallets_file_data
-            evm_addresses_data = evm_addresses_file_data
+            proxy_data = None
+
+        return proxy_data
+
+    def get_wallets(self,
+                    aptos_wallets_data=None,
+                    evm_addresses_data=None,
+                    proxy_data=None) -> Union[List[WalletData], None]:
 
         if not aptos_wallets_data:
             return None
-
         all_evm_addresses = []
         if evm_addresses_data:
-            all_evm_addresses = [evm_address for evm_address in evm_addresses_data if len(evm_address) == 42]
+            all_evm_addresses = [addr for addr in evm_addresses_data if len(addr) == self.EVM_ADDRESS_LENGTH]
+
+        all_proxy_data = []
+        if proxy_data:
+            all_proxy_data = [proxy for proxy in proxy_data]
 
         all_wallets = []
         for index, wallet in enumerate(aptos_wallets_data):
             evm_pair_address = all_evm_addresses[index] if len(all_evm_addresses) > index else None
-            if len(wallet) == self.wallet_length:
-                wallet_data = WalletData(wallet=wallet,
-                                         evm_pair_address=evm_pair_address)
-                all_wallets.append(wallet_data)
-
-            if len(wallet) > self.wallet_length:
-                try:
-                    split_wallet = wallet.split(":")
-                    if len(split_wallet[0]) != self.wallet_length:
-                        continue
-
-                    if len(split_wallet) == 3:
-                        proxy_data = ProxyData(host=split_wallet[1],
-                                               port=split_wallet[2])
-                        wallet_data = WalletData(wallet=split_wallet[0],
-                                                 proxy=proxy_data,
-                                                 evm_pair_address=evm_pair_address)
-                        all_wallets.append(wallet_data)
-
-                    if len(split_wallet) == 5:
-                        proxy_data = ProxyData(host=split_wallet[1],
-                                               port=split_wallet[2],
-                                               username=split_wallet[3],
-                                               password=split_wallet[4],
-                                               auth=True)
-                        wallet_data = WalletData(wallet=split_wallet[0],
-                                                 proxy=proxy_data,
-                                                 evm_pair_address=evm_pair_address)
-                        all_wallets.append(wallet_data)
-
-                except Exception as e:
-                    print(e)
-                    return None
+            paired_proxy_data = all_proxy_data[index] if len(all_proxy_data) > index else None
+            if len(wallet) != self.APTOS_KEY_LENGTH:
+                continue
+            proxy_data = self._extract_valid_proxy_data_from_str(paired_proxy_data)
+            wallet_data = WalletData(wallet=wallet,
+                                     evm_pair_address=evm_pair_address,
+                                     proxy=proxy_data)
+            all_wallets.append(wallet_data)
 
         return all_wallets
+
+    def get_wallets_from_files(self):
+        aptos_wallets_data = self.read_data_from_txt_file(APTOS_WALLETS_FILE)
+        evm_addresses_data = self.read_data_from_txt_file(EVM_ADDRESSES_FILE)
+        proxy_data = self.read_data_from_txt_file(PROXY_FILE)
+        return self.get_wallets(aptos_wallets_data=aptos_wallets_data,
+                                evm_addresses_data=evm_addresses_data,
+                                proxy_data=proxy_data)
