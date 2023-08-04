55fdb793e0749996743185a5e5aaa27ebe0831a7
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-04 16:37:56 +0500

    ADD random_dst_coin field

--- src/schemas/pancake.py
+++ src/schemas/pancake.py
@@ -7,5 +7,6 @@ class PancakeConfigSchema(TransferBase):
     module_name: str = "pancake"
     coin_to_swap: Union[str, None] = ""
     coin_to_receive: Union[str, None] = ""
+    random_dst_coin: Union[bool, str] = False
     send_all_balance: Union[bool, str] = False
     slippage: Union[int, str] = 0
