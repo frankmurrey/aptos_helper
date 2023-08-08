bcb1333c33e26ed315104c2a5ef804503f0a1f58
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-08 20:21:47 +0500

    ADD mobile proxy ip rotation

--- modules/module_executor.py
+++ modules/module_executor.py
@@ -8,7 +8,7 @@ from src.file_manager import FileManager
 from src.config_manager import print_config
 from src.proxy_manager import ProxyManager
 from src.schemas.wallet_data import WalletData
-from src.storage import WalletsStorage
+from src.storage import Storage
 from src.templates.templates import Templates
 from src.action_logger import log_all_actions_to_xlsx
 
@@ -32,15 +32,14 @@ from aptos_sdk.account import Account
 
 class ModuleExecutor:
     file_manager: FileManager
-    proxy_manager: ProxyManager
     wallets: List[WalletData]
 
     def __init__(self, config):
-        self.proxy_manager = ProxyManager()
         self.config = config
         self.module_name = config.module_name
-        self.wallets_storage = WalletsStorage()
-        self.wallets = self.wallets_storage.get_wallets_data()
+        self.storage = Storage()
+        self.app_config = self.storage.get_app_config()
+        self.wallets = self.storage.get_wallets_data()
         self.templates = Templates()
 
     def get_delay_sec(self, execution_status: bool) -> int:
@@ -59,7 +58,7 @@ class ModuleExecutor:
     def start(self):
         print_config(config=self.config)
 
-        if self.wallets_storage.get_shuffle_wallets() is True:
+        if self.storage.get_shuffle_wallets() is True:
             wallets = self.wallets
             random.shuffle(wallets)
         else:
@@ -72,14 +71,16 @@ class ModuleExecutor:
             logger.info(f"PK - {self.blur_private_key(wallet_data.wallet)}")
 
             execute_status: bool = self.execute_module(wallet_data=wallet_data,
-                                                       base_url=self.wallets_storage.get_rpc_url())
+                                                       base_url=self.storage.get_rpc_url())
 
             if self.config.test_mode is True and index == 2:
                 logger.info(f"Test mode enabled, process is finished\n")
                 break
 
             if index == wallets_amount - 1:
-                log_all_actions_to_xlsx()
+                if self.app_config.preserve_logs is True:
+                    log_all_actions_to_xlsx()
+
                 logger.info(f"Process is finished\n")
                 break
 
@@ -96,33 +97,42 @@ class ModuleExecutor:
                        base_url: str) -> Union[bool, None]:
 
         execution_status = None
-        proxy: dict = self.proxy_manager.get_proxy(proxy_data=wallet_data.proxy)
-        if proxy:
-            is_proxy_valid = self.proxy_manager.ping(proxy=proxy)
-            if is_proxy_valid is False:
+        proxy_data = wallet_data.proxy
+        proxy_manager = ProxyManager(proxy_data=proxy_data)
+        proxies = proxy_manager.get_proxy()
+
+        if proxy_data:
+            if proxy_data.is_mobile is True and self.app_config.mobile_proxy_rotation is True:
+                rotation_link = self.app_config.mobile_proxy_rotation_link
+                rotate_status = proxy_manager.rotate_mobile_proxy(rotation_link)
+                if rotate_status is False:
+                    return False
+
+            current_ip = proxy_manager.get_ip()
+            if current_ip is None:
                 logger.error(f"Proxy {wallet_data.proxy.host}:{wallet_data.proxy.port} is not valid or bad auth params")
                 return False
             else:
-                logger.info(f"Proxy valid, using {wallet_data.proxy.host}:{wallet_data.proxy.port}")
+                logger.info(f"Proxy valid, using {wallet_data.proxy.host}:{wallet_data.proxy.port} (ip: {current_ip})")
 
         if self.module_name == 'pancake':
             pancake = PancakeSwap(base_url=base_url,
                                   config=self.config,
-                                  proxies=proxy)
+                                  proxies=proxies)
             swap_status = pancake.make_swap(private_key=wallet_data.wallet)
             execution_status = swap_status
 
         elif self.module_name == "aptos_bridge_claim":
             claimer = BridgedTokenClaimer(base_url=base_url,
                                           config=self.config,
-                                          proxies=proxy)
+                                          proxies=proxies)
             claim_status = claimer.claim_batch(private_key=wallet_data.wallet)
             execution_status = claim_status
 
         elif self.module_name == "aptos_bridge":
             bridge = AptosBridge(base_url=base_url,
                                  config=self.config,
-                                 proxies=proxy)
+                                 proxies=proxies)
             bridge_status = bridge.send_transaction(private_key=wallet_data.wallet,
                                                     receiver_address=wallet_data.evm_pair_address)
             execution_status = bridge_status
@@ -130,35 +140,35 @@ class ModuleExecutor:
         elif self.module_name == "able_mint":
             mint = AbleFinance(base_url=base_url,
                                config=self.config,
-                               proxies=proxy)
+                               proxies=proxies)
             mint_status = mint.send_mint_transaction(private_key=wallet_data.wallet)
             execution_status = mint_status
 
         elif self.module_name == "able_redeem":
             redeem = AbleFinance(base_url=base_url,
                                  config=self.config,
-                                 proxies=proxy)
+                                 proxies=proxies)
             redeem_status = redeem.send_redeem_transaction(private_key=wallet_data.wallet)
             execution_status = redeem_status
 
         elif self.module_name == "thala_add_liquidity":
             thala = Thala(base_url=base_url,
                           config=self.config,
-                          proxies=proxy)
+                          proxies=proxies)
             add_liq_status = thala.send_add_liquidity_transaction(private_key=wallet_data.wallet)
             execution_status = add_liq_status
 
         elif self.module_name == "thala_remove_liquidity":
             thala = Thala(base_url=base_url,
                           config=self.config,
-                          proxies=proxy)
+                          proxies=proxies)
             remove_liq_status = thala.send_remove_liquidity_transaction(private_key=wallet_data.wallet)
             execution_status = remove_liq_status
 
         elif self.module_name == "liquidityswap_swap":
             liq_swap = Swap(base_url=base_url,
                             config=self.config,
-                            proxies=proxy)
+                            proxies=proxies)
 
             swap_status = liq_swap.make_swap(private_key=wallet_data.wallet)
             execution_status = swap_status
@@ -166,28 +176,28 @@ class ModuleExecutor:
         elif self.module_name == "liquidityswap_add_liquidity":
             liq_swap = LSLiquidity(base_url=base_url,
                                    config=self.config,
-                                   proxies=proxy)
+                                   proxies=proxies)
             add_liq_status = liq_swap.send_add_liquidity_transaction(private_key=wallet_data.wallet)
             execution_status = add_liq_status
 
         elif self.module_name == "liquidityswap_remove_liquidity":
             liq_swap = LSLiquidity(base_url=base_url,
                                    config=self.config,
-                                   proxies=proxy)
+                                   proxies=proxies)
             remove_liq_status = liq_swap.send_remove_liquidity_transaction(private_key=wallet_data.wallet)
             execution_status = remove_liq_status
 
         elif self.module_name == "delegate":
             delegate = Delegate(base_url=base_url,
                                 config=self.config,
-                                proxies=proxy)
+                                proxies=proxies)
             delegate_status = delegate.send_delegation_transaction(private_key=wallet_data.wallet)
             execution_status = delegate_status
 
         elif self.module_name == "unlock":
             unlock = Unlock(base_url=base_url,
                             config=self.config,
-                            proxies=proxy)
+                            proxies=proxies)
             unlock_status = unlock.send_unlock_transaction(private_key=wallet_data.wallet)
             execution_status = unlock_status
 
