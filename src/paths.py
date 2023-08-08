6699292cf41bb6a9344d8175d941cf49ebbb56a3
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-08 20:23:17 +0500

    ADD APP_CONFIG_FILE path

--- src/paths.py
+++ src/paths.py
@@ -12,6 +12,7 @@ GUI_IMAGES_DIR = os.path.join(GUI_DIR, 'images')
 APTOS_WALLETS_FILE = os.path.join(MAIN_DIR, "wallets.txt")
 EVM_ADDRESSES_FILE = os.path.join(MAIN_DIR, "evm_addresses.txt")
 PROXY_FILE = os.path.join(MAIN_DIR, "proxy.txt")
+APP_CONFIG_FILE = os.path.join(MAIN_DIR, "app_config.json")
 
 DARK_MODE_LOGO_IMG = os.path.join(GUI_IMAGES_DIR, 'dark_mode_logo.png')
 LIGHT_MODE_LOGO_IMG = os.path.join(GUI_IMAGES_DIR, 'light_mode_logo.png')
@@ -23,5 +24,6 @@ class TempFiles:
         self.TOKENS_JSON_FILE = os.path.join(CONTRACTS_DIR, "tokens.json")
         self.RPC_URLS_JSON_FILE = os.path.join(CONTRACTS_DIR, "rpc_urls.json")
         self.LOGS_DIR = LOGS_DIR
+        self.APP_CONFIG_FILE = APP_CONFIG_FILE
 
 
