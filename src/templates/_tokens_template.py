3b4c34d484548cc97b9700526129feef96f3ba1d
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-09 17:08:11 +0500

    ADD liquid_swap fields

--- src/templates/_tokens_template.py
+++ src/templates/_tokens_template.py
@@ -3,7 +3,8 @@ TOKENS_DATA = [
         "symbol": "aptos",
         "contract": "0x1::aptos_coin::AptosCoin",
         "is_pancake_available": True,
-        "is_abel_available": True
+        "is_abel_available": True,
+        "is_liquid_swap_available": True
     },
 
     {
@@ -12,7 +13,8 @@ TOKENS_DATA = [
         "is_pancake_available": True,
         "aptos_bridge_handle": "0x8957ca6dab8475411756bfbb17e93534cc15bd942c7f275234be5381c3a48dff",
         "is_abel_available": True,
-        "is_thala_available": True
+        "is_thala_available": True,
+        "is_liquid_swap_available": True
     },
 
     {
@@ -21,7 +23,8 @@ TOKENS_DATA = [
         "is_pancake_available": True,
         "aptos_bridge_handle": "0x2e46d98566716dc078ca858162169242fd0e45d7f2f8fb4799767c17e0af99bd",
         "is_abel_available": True,
-        "is_thala_available": True
+        "is_thala_available": True,
+        "is_liquid_swap_available": True
     },
 
     {
@@ -29,6 +32,7 @@ TOKENS_DATA = [
         "contract": "0x159df6b7689437016108a019fd5bef736bac692b6d4a1f10c941f6fbb9a74ca6::oft::CakeOFT",
         "is_pancake_available": True,
         "aptos_bridge_handle": "0xba4efb408cd949a289e2966cf7f94a5910817772881c175a1a71748ef442638",
-        "is_abel_available": True
+        "is_abel_available": True,
+        "is_liquid_swap_available": True
     }
 ]
