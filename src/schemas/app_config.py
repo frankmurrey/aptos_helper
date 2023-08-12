6c32d9a7efa1ad1c5cf60cd52a7c86e002381ed3
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-12 18:36:19 +0500

    ADD aptos_rpc_url field

--- src/schemas/app_config.py
+++ src/schemas/app_config.py
@@ -7,3 +7,4 @@ class AppConfigSchema(BaseModel):
     preserve_logs: bool = True
     mobile_proxy_rotation: bool = False
     mobile_proxy_rotation_link: Union[str, None] = ""
+    aptos_rpc_url: str = "https://rpc.ankr.com/http/aptos/v1"
