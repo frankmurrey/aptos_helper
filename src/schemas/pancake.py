c712a6d44196516622986848f9c192cbeb3f8cd0
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-16 09:29:14 +0500

    ADD send_percent_balance field

--- src/schemas/pancake.py
+++ src/schemas/pancake.py
@@ -9,4 +9,5 @@ class PancakeConfigSchema(TransferBase):
     coin_to_receive: Union[str, None] = ""
     random_dst_coin: Union[bool, str] = False
     send_all_balance: Union[bool, str] = False
+    send_percent_balance: Union[bool, str] = False
     slippage: Union[int, str] = 0
