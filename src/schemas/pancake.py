3e7b78ca0926ca5071aba704af0c1584fae65de3
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-22 19:42:59 +0500

    ADD compare_with_actual_price, max_price_difference fields

--- src/schemas/pancake.py
+++ src/schemas/pancake.py
@@ -10,4 +10,6 @@ class PancakeConfigSchema(TransferBase):
     random_dst_coin: Union[bool, str] = False
     send_all_balance: Union[bool, str] = False
     send_percent_balance: Union[bool, str] = False
+    compare_with_actual_price: Union[bool, str] = True
+    max_price_difference: Union[int, str] = 3
     slippage: Union[int, str] = 0
