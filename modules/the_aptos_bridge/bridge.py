921822eb43bbf15617167e6a793d6ee73856f72b
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-08-22 19:41:42 +0500

    UPD txn_status returns tupple

--- modules/the_aptos_bridge/bridge.py
+++ modules/the_aptos_bridge/bridge.py
@@ -148,7 +148,7 @@ class AptosBridge(AptosBase):
 
     def send_transaction(self,
                          private_key: str,
-                         receiver_address: str):
+                         receiver_address: str) -> bool:
         sender_account = self.get_account(private_key=private_key)
         txn_payload = self.build_transaction_payload(sender_account=sender_account,
                                                      receiver_address=receiver_address)
@@ -165,5 +165,7 @@ class AptosBridge(AptosBase):
             txn_payload=txn_payload,
             txn_info_message=txn_info_message
         )
+
+        txn_status, txn_status_message = txn_status
 
         return txn_status
