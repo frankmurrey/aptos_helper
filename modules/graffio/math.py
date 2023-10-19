9a2c724106654d6cd39720729e3d4809239e49b6
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-10-19 13:18:38 +0500

    HFIX

--- modules/graffio/math.py
+++ modules/graffio/math.py
@@ -1,39 +1,6 @@
 from typing import Union
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def get_random_letter(n_max: int) -> Union[None, tuple]:
-    max_pixels = n_max
-    letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    np.random.shuffle(letters)
-
-    for letter in letters:
-        fig, ax = plt.subplots(figsize=(0.5, 0.5))
-        ax.axis('off')
-        ax.text(0.5, 0.5, letter, ha='center', va='center', fontsize=20)
-
-        fig.canvas.draw()
-
-        data = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8).reshape(
-            fig.canvas.get_width_height()[::-1] + (4,))
-        plt.close(fig)
-
-        gray = np.dot(data[..., :3], [0.299, 0.587, 0.114])
-        binary = gray < 128
-
-        y, x = np.where(binary)
-
-        random_prefix = np.random.randint(0, 900)
-
-        y = [int(i) + random_prefix for i in y]
-        x = [int(i) + random_prefix for i in x]
-
-        if len(x) <= max_pixels:
-            return list(x), list(y)
-
-    return None
 
 
 def get_random_coord(max_pixels: int) -> tuple:
