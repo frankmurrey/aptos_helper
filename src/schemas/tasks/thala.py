290000ec11e6e94759f39ba5c5965c437cd5b905
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-12-27 14:52:04 +0500

    ADD swap task

--- src/schemas/tasks/thala.py
+++ src/schemas/tasks/thala.py
@@ -1,12 +1,24 @@
 from typing import Callable
 from pydantic import Field
 
+from src.schemas.tasks.base.swap import SwapTaskBase
 from src.schemas.tasks.base.add_liquidity import AddLiquidityTaskBase
 from src.schemas.tasks.base.remove_liquidity import RemoveLiquidityTaskBase
+from src.schemas.tasks.base.supply import SupplyTaskBase
+from src.schemas.tasks.base.withdraw import WithdrawTaskBase
+from modules.thala.swap import ThalaSwap
 from modules.thala.liquidity import ThalaAddLiquidity
 from modules.thala.liquidity import ThalaRemoveLiquidity
+from modules.thala.supply import ThalaSupply
+from modules.thala.supply import ThalaWithdraw
 
 from src import enums
+
+
+class ThalaSwapTask(SwapTaskBase):
+    module_name: enums.ModuleName = enums.ModuleName.THALA
+    module_type: enums.ModuleType = enums.ModuleType.SWAP
+    module: Callable = Field(default=ThalaSwap)
 
 
 class ThalaRemoveLiquidityTask(RemoveLiquidityTaskBase):
@@ -24,3 +36,21 @@ class ThalaAddLiquidityTask(AddLiquidityTaskBase):
     class Config:
         arbitrary_types_allowed = True
 
+
+class ThalaWithdrawTask(WithdrawTaskBase):
+    module_name = enums.ModuleName.THALA
+    module_type = enums.ModuleType.WITHDRAW
+    module: Callable = Field(default=ThalaWithdraw)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class ThalaSupplyTask(SupplyTaskBase):
+    module_name = enums.ModuleName.THALA
+    module_type = enums.ModuleType.SUPPLY
+    module: Callable = Field(default=ThalaSupply)
+    reverse_action_task: Callable = Field(default=ThalaWithdrawTask)
+
+    class Config:
+        arbitrary_types_allowed = True
