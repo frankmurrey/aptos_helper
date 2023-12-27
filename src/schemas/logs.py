8045b82940852d0cb0f892c4aa12cb40dd7b7b59
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2023-12-27 14:51:05 +0500

    UPD imports

--- src/schemas/logs.py
+++ src/schemas/logs.py
@@ -1,7 +1,19 @@
-from typing import Union
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from datetime import timedelta
+from typing import Optional, Dict, Union
+from typing import TYPE_CHECKING
+
+from loguru import logger
+from pydantic import BaseModel
+
 from src import enums
 
-from pydantic import BaseModel
+if TYPE_CHECKING:
+    from loguru._recattrs import RecordFile
+    from loguru._recattrs import RecordLevel
+    from loguru._recattrs import RecordProcess
+    from loguru._recattrs import RecordThread
 
 
 class WalletActionSchema(BaseModel):
@@ -12,5 +24,32 @@ class WalletActionSchema(BaseModel):
     wallet_address: str = None
     proxy: Union[str, None] = None
     is_success: Union[bool, None] = None
-    status: str = None
     transaction_hash: str = None
+
+    status: str = None
+    traceback: str = None
+
+    def set_error(self, message: str):
+        logger.error(message)
+        self.status = message
+        self.is_success = False
+
+
+@dataclass
+class LogRecord:
+    elapsed: timedelta
+    exception: Optional[Exception]
+    extra: Dict
+    file: "RecordFile"
+    function: str
+    level: "RecordLevel"
+    line: int
+    message: str
+    module: str
+    name: str
+    process: "RecordProcess"
+    thread: "RecordThread"
+    time: datetime
+
+    def dict(self):
+        return asdict(self)
