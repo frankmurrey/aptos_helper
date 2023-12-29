07261879e4845e733e77a433f0962b3cf5b1d70e
vasyapc <sekachgamer@gmail.com>
2023-12-29 08:04:52 +0300

    ADD merkle tasks

--- src/schemas/tasks/merkle.py
+++ src/schemas/tasks/merkle.py
@@ -5,18 +5,35 @@ from src import enums
 
 from src.schemas.tasks.base.base import TaskBase
 from src.schemas import validation_mixins
-from modules.merkle.place_order import MerklePlaceOrder
+from modules.merkle.place_order import MerklePlaceOpenOrder
+from modules.merkle.place_order import MerklePlaceCancelOrder
 
 
-class MerklePlaceOrderTask(
+class MerklePlaceCancelOrderTask(
+    TaskBase,
+    validation_mixins.SlippageValidationMixin
+):
+    module_name: enums.ModuleName = enums.ModuleName.MERKLE
+    module_type: enums.ModuleType = enums.ModuleType.PLACE_CANCEL_ORDER
+    module: Callable = Field(default=MerklePlaceCancelOrder)
+
+    coin_x: str = "usdc"
+
+    slippage: float = 0.5
+    use_referral: bool = False
+
+    order_type: enums.OrderType = enums.OrderType.LONG
+
+
+class MerklePlaceOpenOrderTask(
     TaskBase,
     validation_mixins.MinMaxAmountOutValidationMixin,
     validation_mixins.SlippageValidationMixin
 ):
     module_name: enums.ModuleName = enums.ModuleName.MERKLE
-    module_type: enums.ModuleType = enums.ModuleType.PLACE_ORDER
-    module: Callable = Field(default=MerklePlaceOrder)
-    reverse_action_task: Callable = Field(default=None)
+    module_type: enums.ModuleType = enums.ModuleType.PLACE_OPEN_ORDER
+    module: Callable = Field(default=MerklePlaceOpenOrder)
+    reverse_action_task: Callable = Field(default=MerklePlaceCancelOrderTask)
 
     coin_x: str = "usdc"
 
@@ -24,9 +41,16 @@ class MerklePlaceOrderTask(
 
     min_amount_out: float = 2
     max_amount_out: float = 2
+
+    pseudo_order: bool = False
+    reverse_action: bool = False
     use_referral: bool = False
 
     order_type: enums.OrderType = enums.OrderType.LONG
 
     class Config:
         arbitrary_types_allowed = True
+
+    @property
+    def action_info(self):
+        return self.order_type.value.title()
