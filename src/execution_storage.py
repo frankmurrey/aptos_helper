b19a24baa7e1ffcc0f42d4180b7762dcfb5d92d0
Franky Murrey <53918055+frankmurrey@users.noreply.github.com>
2024-03-14 12:17:09 +0500

    ADD singltone

--- src/execution_storage.py
+++ src/execution_storage.py
@@ -3,32 +3,33 @@ from uuid import UUID
 
 
 class ExecutionStorage:
-    _instance = None
+    __instance = None
+
+    class __Singleton:
+
+        def __init__(self):
+            self.__pre_execution_data: Dict[str, dict] = {}
+
+        @property
+        def pre_execution_data(self) -> Dict[str, dict]:
+            return self.__pre_execution_data
+
+        def get_by_wallet_id_and_task_id(
+                self,
+                wallet_id: UUID,
+                task_id: UUID
+        ) -> Optional[dict]:
+            return self.__pre_execution_data.get(f"{wallet_id}_{task_id}")
+
+        def set_pre_execution_data(
+                self,
+                wallet_id: UUID,
+                task_id: UUID,
+                data: dict
+        ):
+            self.__pre_execution_data[f"{wallet_id}_{task_id}"] = data
 
     def __new__(cls):
-        if not cls._instance:
-            cls._instance = super(ExecutionStorage, cls).__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        self.__pre_execution_data: Dict[str, dict] = {}
-
-    @property
-    def pre_execution_data(self) -> Dict[str, dict]:
-        return self.__pre_execution_data
-
-    def get_by_wallet_id_and_task_id(
-            self,
-            wallet_id: UUID,
-            task_id: UUID
-    ) -> Optional[dict]:
-        return self.__pre_execution_data.get(f"{wallet_id}_{task_id}")
-
-    def set_pre_execution_data(
-            self,
-            wallet_id: UUID,
-            task_id: UUID,
-            data: dict
-    ):
-        self.__pre_execution_data[f"{wallet_id}_{task_id}"] = data
-
+        if not ExecutionStorage.__instance:
+            ExecutionStorage.__instance = ExecutionStorage.__Singleton()
+        return ExecutionStorage.__instance
